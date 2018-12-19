d9b1855098cb481f0d450b37e7f48271e929ff60
Imad <ibelkebir@stuy.edu>
2018-12-19 13:51:47 -0500

    done

--- pipe_networking.c
+++ pipe_networking.c
@@ -9,7 +9,47 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  unlink(ACK);
+  if(mkfifo(ACK, 0666) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Server: WKP made\n");
+  int fifo = open(ACK, O_RDONLY);
+  char name[256];
+  if(read(fifo, name, sizeof(name)) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Server: Got private pipe name: %s\n", name);
+  int upstream = fifo;
+  close(fifo);
+  fifo = open(name, O_WRONLY);
+  if(fifo == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Server: Connected to private pipe\n");
+  *to_client = fifo;
+  if(write(fifo, "I gotchu", strlen("I gotchu")) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  close(fifo);
+  fifo = open(ACK, O_RDONLY);
+  char message[256];
+  if(read(fifo, message, sizeof(message)) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  close(fifo);
+  printf("Server: Got message from client: %s\n", message);
+  if(unlink(ACK) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Server: Deleted WPK\n");
+  return upstream;
 }
 
 
@@ -21,5 +61,48 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  char* name = "private";
+  unlink(name);
+  if(mkfifo(name, 0666) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Client: Created private pipe\n");
+  int fifo = open(ACK, O_WRONLY);
+  if(fifo == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Client: Connected to WKP\n");
+  *to_server = fifo;
+  if(write(fifo, name, strlen(name)) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Client: Wrote to WKP\n");
+  close(fifo);
+  fifo = open(name, O_RDONLY);
+  if(fifo == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  int downstream = fifo;
+  char message[256];
+  if(read(fifo, message, 256) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Client: Got message from server: %s\n", message);
+  close(fifo);
+  fifo = open(ACK, O_WRONLY);
+  if(write(fifo, "Ayo", strlen("Ayo")) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  if(unlink(name) == -1){
+    printf("ERROR: %s\n", strerror(errno));
+    exit(1);
+  }
+  printf("Client: Deleted private pipe\n");
+  return downstream;
 }
