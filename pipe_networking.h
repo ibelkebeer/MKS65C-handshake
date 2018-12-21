ab8f44d116ad230c09599d82ac99d8b76ac4c259
Imad <ibelkebir@stuy.edu>
2018-12-21 15:25:12 -0500

    hmm

--- pipe_networking.h
+++ pipe_networking.h
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
 #include <string.h>
 #include <errno.h>
 #include <math.h>
